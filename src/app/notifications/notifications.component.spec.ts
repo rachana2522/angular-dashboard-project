4d7328297f0abe298c9441495e2842a392dc0465
Rachana priyadarshni samal <77429844+rachana2522@users.noreply.github.com>
2023-08-05 15:14:01 +0530

    Update notifications.component.spec.ts

--- src/app/notifications/notifications.component.spec.ts
+++ src/app/notifications/notifications.component.spec.ts
@@ -1,3 +1,4 @@
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotificationsComponent } from './notifications.component';
@@ -13,6 +14,7 @@ describe('NotificationsComponent', () => {
     .compileComponents();
   }));
 
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(NotificationsComponent);
     component = fixture.componentInstance;
